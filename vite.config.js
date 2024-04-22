d860fcd968c14a606d06dbcad2bacd3209f94c9a
Aswin-00 <aswingirish25@gmail.com>
2024-04-22 21:58:18 +0530

    last

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/reat_github",
+  // base: "/reat_github/",
   plugins: [react()],
 })
